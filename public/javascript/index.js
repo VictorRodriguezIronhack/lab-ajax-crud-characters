95ded8effd347a2b5a9d0f4a8aee29156b2f07d4
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-06-30 08:51:58 +0200

    ENRIQUE VOZMEDIANO

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,11 +1,43 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler();
+
+
+function displayCharacterFromAPI(elem) {
+  let characters = `
+    <div class="character-info">
+     <div class="name">${elem.name}</div>
+      <div class="occupation">${elem.occupation}</div> 
+      <div class="cartoon">${elem.cartoon}</div>
+       <div class="weapon">${elem.weapon}</div>
+        <div class="weapon">${elem.id}</div>
+         </div> `
+  document.querySelector('.characters-container').innerHTML = characters
+}
+
+
+
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI
+      .getFullList()
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
   });
 
+
+
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+
+    const input = document.querySelector('[name="character-id"]')
+    const id = input.value
+
+    charactersAPI
+      .getOneRegister(id)
+      .then(response => {
+        displayCharacter
+
+      })
 
   });
 
